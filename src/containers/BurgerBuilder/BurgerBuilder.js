baad6b7f6d86f0b19fc1f1b37fbddd30bfc5eb56
SHEFFmachine <sheff@sheffmachine.com>
2018-05-14 14:19:45 -0700

    Modal implemented

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -30,7 +30,11 @@ class BurgerBuilder extends Component {
   }
 
   purchaseHandler() {
-    this.setState({purchasing: true});
+    this.setState({ purchasing: true });
+  }
+
+  purchaseCancelHandler() {
+    this.setState({ purchasing: false });
   }
 
   updatePurchaseState(ingredients) {
@@ -73,7 +77,7 @@ class BurgerBuilder extends Component {
 
     return (
       <Aux>
-        <Modal show={this.state.purchasing}>
+        <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler.bind(this)}>
           <OrderSummary ingredients={this.state.ingredients} />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
