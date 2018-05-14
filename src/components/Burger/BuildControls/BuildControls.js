3259cd0b2fa60e4460c7bff15291565d63ff1597
SHEFFmachine <sheff@sheffmachine.com>
2018-05-14 11:17:44 -0700

    burger controls complete

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import BuildControl from './BuildControl/BuildControl';
+import classes from './BuildControls.css';
 
-const buildControls = () => {
-  
-}
+const CONTROLS = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' },
+];
+
+const buildControls = () => (
+  <div className={classes.BuildControls}>
+    {CONTROLS.map((control, index) => (
+      <BuildControl key={index} label={control.label} type={control.type} />
+    ))}
+  </div>
+);
 
 export default buildControls;
