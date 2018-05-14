01357ad792c9a4f4dda9911cecb4832b9114ff18
SHEFFmachine <sheff@sheffmachine.com>
2018-05-14 10:50:19 -0700

    burger builder is now dynamic

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import BurgerIngredient from './BurgerIngredients/BurgerIngredient';
 
 
 const burger = (props) => {
+
+  /**
+   * Here we flattan the ingredients array.
+   * This does not impact react's ability to render the components,
+   * as react is able to render nested array jsx.
+   */
   let transformedIngredients = Object.keys(props.ingredients).map(ingredient => {
     return [...Array(props.ingredients[ingredient])].map((_, i) => {
       return <BurgerIngredient key={ingredient + i} type={ingredient} />;
-    })
-  });
+    });
+  }).reduce((arr, el) => {
+    return arr.concat(el);
+  }, []);
+
+  if (transformedIngredients.length < 1) {
+    transformedIngredients = <p>Please start adding ingredients!</p>;
+  }
 
   return (
     <div className={classes.Burger}>
