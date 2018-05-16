51cd0ab2659a1dcb1cde8a3240cb37cad1a3161a
SHEFFmachine <sheff@sheffmachine.com>
2018-05-15 22:02:48 -0700

    order with error handling

---
+++ src/containers/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Burger from '../../../components/Burger/Burger';
+import Button from '../../../components/UI/Button/Button';
+import classes from './CheckoutSummary.css';
+
+const checkoutSummary = (props) => (
+  <div className={classes.CheckoutSummary}>
+    <h1>We hope it tastes well!</h1>
+    <div style={{width: '100%', margin: 'auto'}}>
+      <Burger ingredients={props.ingredients}/>
+    </div>
+    <Button btnType="Danger" clicked/>
+    <Button btnType="Success" clicked/>
+  </div>
+);
+
+export default checkoutSummary;
