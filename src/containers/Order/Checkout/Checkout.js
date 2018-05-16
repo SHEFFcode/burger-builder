d7cf1ea83966c11a0cdda09e7bfb1c82c08ace39
SHEFFmachine <sheff@sheffmachine.com>
2018-05-16 11:35:45 -0700

    Checkout page complete

--- src/containers/Order/Checkout/Checkout.js
+++ src/containers/Order/Checkout/Checkout.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 import CheckoutSummary from '../CheckoutSummary/CheckoutSummary';
+import { Route } from 'react-router-dom';
+import ContactData from './ContactData/ContactData';
 
 class Checkout extends Component {
   state = {
-    ingredients: {
-      salad: 1,
-      meat: 1,
-      cheese: 1,
-      bacon: 1
+    ingredients: null,
+    totalPrice: 0
+  }
+
+  componentWillMount() {
+    const query = new URLSearchParams(this.props.location.search);
+    const ingredients = {};
+    let totalPrice = 0;
+    for (let param of query.entries()) {
+      if (param[0] === 'price') {
+        totalPrice = param[0];
+      } else {
+        ingredients[param[0]] = +param[1];
+      }
     }
+    this.setState({ ingredients, totalPrice });
   }
 
   checkoutCancelledHandler() {
@@ -22,10 +34,19 @@ class Checkout extends Component {
   render() {
     return (
       <div>
-        <CheckoutSummary 
-          ingredients={this.state.ingredients} 
+        <CheckoutSummary
+          ingredients={this.state.ingredients}
           checkoutCancelled={this.checkoutCancelledHandler.bind(this)}
           checkoutContinued={this.checkoutContinuedHandler.bind(this)}
+        />
+        <Route 
+          path={`${this.props.match.path}/contact-data`} 
+          render={(props) => (
+            <ContactData 
+              {...props}
+              ingredients={this.state.ingredients}
+              totalPrice={this.state.totalPrice}
+            />)} 
         />
       </div>
     );
