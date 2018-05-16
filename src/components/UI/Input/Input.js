e45c6a5b04a9a7f43e83c1d54a2c80050a396f20
SHEFFmachine <sheff@sheffmachine.com>
2018-05-16 14:15:35 -0700

    set up input options

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -11,6 +11,17 @@ export default (props) => {
     case 'textarea':
       inputEl = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value}></textarea>
       break;
+    case 'select':
+      inputEl = (
+        <select 
+          className={classes.InputElement} 
+          value={props.value}
+        >
+          {props.elementConfig.options.map(option => (
+            <option key={option.value} value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>)
+      break;
     default:
       inputEl = <input className={classes.InputElement} {...props.elementConfig} value={props.value}/>
       break;
